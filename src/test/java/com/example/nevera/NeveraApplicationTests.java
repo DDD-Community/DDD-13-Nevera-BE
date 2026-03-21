c8768c2167a3cf9487ea56df070d2cc37b6ad948
spark2357 <psbted@naver.com>
2026-03-21 15:06:42 +0900

    initial commit

---
+++ src/test/java/com/example/nevera/NeveraApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.nevera;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NeveraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
