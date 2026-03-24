9d313d32b8eaca6ef00ad266fb0a3815cd0b4dfa
세란 <tjtpfks@gmail.com>
2026-03-24 16:07:52 +0900

    [Test] Add controller to verify global response and error handling

--- src/main/java/com/example/nevera/controller/TestController.java
+++ src/main/java/com/example/nevera/controller/TestController.java
@@ -1,5 +1,7 @@
 package com.example.nevera.controller;
 
+import com.example.nevera.common.exception.BusinessException;
+import com.example.nevera.common.exception.ErrorCode;
 import com.example.nevera.entity.TestEntity;
 import com.example.nevera.service.TestService;
 import lombok.RequiredArgsConstructor;
@@ -7,7 +9,9 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @RestController
 @RequiredArgsConstructor
@@ -18,5 +22,20 @@ public class TestController {
     @GetMapping("/dbtest")
     public List<TestEntity> getAllPosts() {
         return testService.findAllPosts();
+    }
+
+    @GetMapping("/success")
+    public Map<String, String> testSuccess() {
+
+        Map<String, String> data = new HashMap<>();
+        data.put("message", "성공했어요!");
+        return data;
+    }
+
+    @GetMapping("/error")
+    public void testError() {
+
+        throw new BusinessException(ErrorCode.DUPLICATE_EMAIL);
+        // 결과: HTTP 상태코드 409 + { "result": null, "error": { "code": 409, "message": "이미 사용 중인 이메일입니다." } }
     }
 }
