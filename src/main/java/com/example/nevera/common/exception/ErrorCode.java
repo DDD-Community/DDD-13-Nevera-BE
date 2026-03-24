ce0b3deef7c3b2aac496ce473a2376133072e35d
spark2357 <psbted@naver.com>
2026-03-24 21:43:44 +0900

    [Refactor] /dbtest 응답 형식 변경 및 USER_NOT_FOUND 에러 추가

--- src/main/java/com/example/nevera/common/exception/ErrorCode.java
+++ src/main/java/com/example/nevera/common/exception/ErrorCode.java
@@ -2,7 +2,8 @@ package com.example.nevera.common.exception;
 
 public enum ErrorCode {
 
-    DUPLICATE_EMAIL(409, "이미 사용 중인 이메일입니다.");
+    DUPLICATE_EMAIL(409, "이미 사용 중인 이메일입니다."),
+    USER_NOT_FOUND(404, "해당 ID를 가진 유저를 찾을 수 없습니다.");
 
     private final int status;
     private final String message;
