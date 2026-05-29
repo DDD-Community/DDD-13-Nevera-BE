75ffcc42e5198177b41562957f34c6077a3a88d6
세란 <tjtpfks@gmail.com>
2026-05-29 17:16:01 +0900

    [feat] OCR SSE 진행률 push 동기 방식으로 변경 및 구조 개선

--- src/main/java/com/example/nevera/NeveraApplication.java
+++ src/main/java/com/example/nevera/NeveraApplication.java
@@ -2,12 +2,14 @@ package com.example.nevera;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 import java.util.TimeZone;
 
 @SpringBootApplication
 @EnableScheduling
+@EnableAsync
 public class NeveraApplication {
 
 	public static void main(String[] args) {
